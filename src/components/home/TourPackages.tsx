d0f199bb92f79f84a18aaea2186c8e921a6939cd
Noel2002 <64746276+Noel2002@users.noreply.github.com>
2025-01-05 20:21:21 +0800

    Homepage (#1)
    * feat: created home page
    
    * fx: fixed build error
    
    ---------
    
    Co-authored-by: Noel2002 <berwanoel@gmail>

---
+++ src/components/home/TourPackages.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Section from '../shared/Section';
+import PackageCard from '../shared/PackageCard';
+import { PACKAGES } from '@/lib/data';
+import { Button } from '../ui/button';
+
+const TourPackages = () => {
+  return (
+    <Section className='space-y-6'>
+        <h1 className=' text-3xl font-bold'>Tour Packages</h1>
+        <p className=' max-w-[800px] font-light text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+        </p>
+        <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+          {
+            PACKAGES.map((pkg, index) => (
+              <PackageCard
+                key={index}
+                title={pkg.title}
+                description={pkg.description}
+                price={pkg.price}
+                image={pkg.image}
+                rating={pkg.rating}
+              />
+            ))
+          }
+        </div>
+        <div className='flex justify-center'>
+          <Button className=' px-8 border border-black bg-transparent text-black hover:bg-black hover:text-white mx-auto'>
+            See more
+          </Button>
+        </div>
+    </Section>
+  )
+}
+
+export default TourPackages
---
+++ src/lib/data.ts
@@ -0,0 +1,53 @@
+export const PACKAGES = [
+    {
+        title: "The Twin Lakes Adventure",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat.",
+        price: 50,
+        image: "https://res.cloudinary.com/nowo-ltd/image/upload/v1736073731/tourismo/akagera-wildlife_szlwol.jpg",
+        rating: 5,
+    },
+    {
+        title: "Kigali City Tour with a Local Guide and Driver",
+        description: "Set out on an extraordinary journey through the heart of Kigali with our bespoke city tour. Delve into Rwanda's capital, where every moment is crafted to offer a unique and unforgettable experience. From somber reflections at the Genocide Memorial to vibrant markets and hidden treasures, immerse yourself in the essence of this vibrant metropolis.", 
+        price: 100,
+        image: "https://res.cloudinary.com/nowo-ltd/image/upload/v1736074665/tourismo/Visit-Rwanda-Kigali-Centre-Roads-1920x1281_sje6qo.jpg",
+        rating: 4,
+    },
+    {
+        title: "One Day Gorilla Trekking Rwanda & Iby'iwacu Cultural Village",
+        description: "Being a day trip expect to leave Kigali early enough and one way to Volcanoes National Park in Musanze, The Northwestern part of Rwanda. For an estimated 2 - 2.5 hours drive with local experts and friendly driver guide, the transport is assured with a choice 4x4 safari vehicle with wifi connectivity to keep you connected with your lovely ones and enjoying a scenic drive of picturesque countryside.",
+        price: 500,
+        image: "https://res.cloudinary.com/nowo-ltd/image/upload/v1736074837/tourismo/silverback-gorilla-rwanda-bamboo_zwipaf.jpg",
+        rating: 5,
+    },
+]
+
+export const TESTIMONIALS = [
+    {
+        author: {
+            name: "Jon Paterson",
+            photo: "https://res.cloudinary.com/nowo-ltd/image/upload/v1736077727/tourismo/premium_photo-1682144187125-b55e638cf286_vtvsey.jpg",
+            nationality: "England"
+        },
+        ratings: 4,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ipsum ex,  ultrices at magna et, rhoncus iaculis quam. Lorem ipsum dolor sit amet,  consectetur adipiscing elit. ",
+    },
+    {
+        author: {
+            name: "Jane Smith",
+            photo: "https://res.cloudinary.com/nowo-ltd/image/upload/v1736078016/tourismo/photo-1531746020798-e6953c6e8e04_baspau.jpg",
+            nationality: "Canada"
+        },
+        ratings: 4,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ipsum ex,  ultrices at magna et, rhoncus iaculis quam. Lorem ipsum dolor sit amet,  consectetur adipiscing elit. ",
+    },
+    {
+        author: {
+            name: "Johnson Peter",
+            photo: "https://res.cloudinary.com/nowo-ltd/image/upload/v1736077872/tourismo/portrait-african-american-man_23-2149072179_y07it0.jpg",
+            nationality: "United States of America"
+        },
+        ratings: 4,
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ipsum ex,  ultrices at magna et, rhoncus iaculis quam. Lorem ipsum dolor sit amet,  consectetur adipiscing elit. ",
+    },
+]
